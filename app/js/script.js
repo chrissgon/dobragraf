bbccaf72d8fec3fb414b794571bee50793af6a12
Christopher Gonçalves <christopher.goncalves2002@hotmail.com.br>
2021-01-26 23:44:44 -0300

    Desenvolvendo carousel de clientes

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,40 @@
-$(".mobile .btn").click(function(){
-    $(".mobile").toggleClass("active")
-})
+$(".mobile .btn").click(function() {
+  $(".mobile").toggleClass("active");
+});
 
-$(document).click(function(e){
-    let btn = $(".mobile .btn");
-    if(!btn.is(e.target) && btn.has(e.target).length === 0){
-        $(".mobile").removeClass("active")
-    }
-})
+$(document).click(function(e) {
+  let btn = $(".mobile .btn");
+  if (!btn.is(e.target) && btn.has(e.target).length === 0) {
+    $(".mobile").removeClass("active");
+  }
+});
+
+$(".carousel").slick({
+  infinite: false,
+  slidesToShow: 6,
+  slidesToScroll: 6,
+  dots: true,
+  responsive: [
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }]
+});
